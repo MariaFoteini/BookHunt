35ddbb4ceb0121f9271b96a9b9830e41229df108
MariaFoteini <94992867+MariaFoteini@users.noreply.github.com>
2025-03-14 16:52:37 +0200

    Create Book-Component test it works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BookComponent } from './user/book/book.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    BookComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
