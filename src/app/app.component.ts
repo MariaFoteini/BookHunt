eb4515a1d630705988d76e8cb8fa3205949473af
MariaFoteini <94992867+MariaFoteini@users.noreply.github.com>
2025-03-25 16:08:26 +0200

    add header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BookListComponent } from './user/book-list/book-list.component';
 import { SearchBarComponent } from './user/search-bar/search-bar.component';
+import { NavbarComponent } from './user/navbar/navbar.component';
+
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     BookListComponent,
-    SearchBarComponent
+    SearchBarComponent,
+    NavbarComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
