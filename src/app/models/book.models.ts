7a8ad103c67ce1107f477476e9e48801611c9f7b
MariaFoteini <94992867+MariaFoteini@users.noreply.github.com>
2025-04-08 19:38:28 +0300

    Update book model

--- src/app/models/book.models.ts
+++ src/app/models/book.models.ts
@@ -1,10 +1,13 @@
 export interface Book {
     id: string;
     title: string;
-    image: string;
-    authors: string;
-    categories: string;
-    pageCount: number;
+    image: string[];
+    authors: string[];
+    categories: string[];
     publishedDate: string;
     textSnippet: string;
+    description: string;
+    language: string;
+    pageCount: number;
+    previewLink: string;
 };
